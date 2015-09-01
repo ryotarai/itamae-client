ee650bd8ee08257ae39ceef823fad6d53fd284dd
Ryota Arai <ryota-arai@cookpad.com>
2015-09-01 19:34:05 +0900

    Rewrite CLI class.

--- lib/itamae/client/cli.rb
+++ lib/itamae/client/cli.rb
@@ -18,30 +18,80 @@ module Itamae
       method_option :revision, type: :string, required: true
       method_option :dry_run, type: :boolean, default: false
       def apply
-        client = APIClient.new(@options[:server_url])
-
-        revision = client.revision(@options[:revision])
-        execution = client.create_execution(revision_id: revision.id, is_dry_run: @options[:dry_run])
-        host_execution = client.create_host_execution(execution_id: execution.id, host: Socket.gethostbyname(Socket.gethostname).first)
+        prepare_client
+        revision = @client.revision(options[:revision])
+        execution = @client.create_execution(revision_id: revision.id, is_dry_run: options[:dry_run])
+        host_execution = @client.create_host_execution(
+          execution_id: execution.id,
+          status: "pending",
+        )
 
         Runner.new(revision, execution, host_execution, options).run
       end
 
-      desc 'consul', 'handle Consul events'
+      desc 'consul-watch', 'watch Consul events'
       define_run_method_options
-      method_option :once, type: :boolean, default: true, desc: 'for debugging'
-      def consul
-        event = ConsulEvent.last
+      method_option :index_file, type: :string, default: "/tmp/itamae-client-consul-index"
+      method_option :lock_prefix, type: :string, default: "itamae-client"
+      method_option :lock_limit, type: :numeric
+      def consul_watch
+        prepare_client
+        watcher = Consul::EventWatcher.new("http://localhost:8500", options[:index_file])
 
-        unless event
-          puts "no event"
-          exit
+        watcher.watch do |event|
+          begin
+            Itamae::Client.logger.debug "got event: #{event}"
+
+            execution_id = event.payload.fetch('execution_id')
+            execution = @client.execution(execution_id)
+
+            if !execution.is_dry_run && options[:lock_limit]
+              lock = Consul::Lock.new(options[:lock_prefix], options[:lock_limit])
+              Itamae::Client.logger.info "waiting for obtaining lock"
+
+              lock.with_lock do
+                Itamae::Client.logger.info "lock obtained"
+                execute(execution_id)
+              end
+            else
+              execute(execution_id)
+            end
+          rescue => err
+            Itamae::Client.logger.error "#{err}\n#{err.backtrace.join("\n")}"
+            execution.host_execution.mark_as('aborted') if execution
+          end
+        end
+      end
+
+      private
+
+      def prepare_client
+        @client = APIClient.new(options[:server_url])
+      end
+
+      def execute(execution_id)
+        execution = @client.execution(execution_id)
+        revision = execution.revision
+        host_execution = execution.host_execution
+
+        Itamae::Client.logger.debug "execution: #{execution}"
+        Itamae::Client.logger.debug "revision: #{revision}"
+        Itamae::Client.logger.debug "host execution: #{host_execution}"
+
+        unless execution.in_progress?
+          Itamae::Client.logger.error "execution ##{execution.id} is not in progress"
+          return
         end
 
-        client = APIClient.new(@options[:server_url])
-        execution = client.execution(event.payload.fetch('execution_id'))
-        revision = execution.revision
-        host_execution = client.create_host_execution(execution_id: execution.id, host: Socket.gethostbyname(Socket.gethostname).first)
+        if !execution.is_dry_run && !revision.active?
+          Itamae::Client.logger.error "actual run can be done only against an active revision"
+          return
+        end
+
+        unless host_execution
+          Itamae::Client.logger.error "no HostExecution found"
+          return
+        end
 
         Runner.new(revision, execution, host_execution, options).run
       end
